475a1f36c4e0b24368372bce75481dfcfa7fe00b
manny <labimanny@gmail.com>
2023-08-22 04:07:50 +0100

    second

---
+++ car-sales-frontend/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import Login from './login';
+import Logout from './logout';
+import ChangePassword from './ChangePassword';
+
+function App() {
+  return (
+    <Router>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">Car Sales App</Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/logout">Logout</Nav.Link>
+            <Nav.Link href="/change-password">Change Password</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/change-password" element={<ChangePassword />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
