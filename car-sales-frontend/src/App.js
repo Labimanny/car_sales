61a99cf98830a4b956bafdd9247df439ef0d2843
manny <labimanny@gmail.com>
2023-08-25 18:57:57 +0100

    third-commit

--- car-sales-frontend/src/App.js
+++ car-sales-frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Container, Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './login';
 import Logout from './logout';
 import ChangePassword from './ChangePassword';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Dashboard from './Dashboard';
 import PrivateRoute from './PrivateRoute';
 import AddCar from './AddCar';
@@ -12,11 +12,9 @@ import CarList from './CarList';
 import AddSale from './AddSale';
 import SalesList from './SalesList';
 
-
-
 function App() {
   return (
-    <><Router>
+    <Router>
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
           <Navbar.Brand href="/">Car Sales App</Navbar.Brand>
@@ -33,21 +31,48 @@ function App() {
         <Route path="/logout" element={<Logout />} />
         <Route path="/change-password" element={<ChangePassword />} />
         <Route path="/" element={<Navigate to="/login" />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/add-car"
+          element={
+            <PrivateRoute>
+              <AddCar />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/car-list"
+          element={
+            <PrivateRoute>
+              <CarList />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/add-sale"
+          element={
+            <PrivateRoute>
+              <AddSale />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/sales-list"
+          element={
+            <PrivateRoute>
+              <SalesList />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
-    <Router>
-        <Routes>
-          <Route path="/login" component={Login} />
-          <PrivateRoute path="/dashboard" component={Dashboard} />
-          <PrivateRoute path="/add-car" component={AddCar} />
-          <PrivateRoute path="/car-list" component={CarList} />
-          <PrivateRoute path="/add-sale" component={AddSale} />
-          <PrivateRoute path="/sales-list" component={SalesList} />
-          <PrivateRoute path="/change-password" component={ChangePassword} />
-          <Route path="/" element={<Navigate to="/dashboard" />} />
-        </Routes>
-      </Router></>
-
   );
 }
 
